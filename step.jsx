9238553bc2ef175687fb3920e1ad1b3a02c89000
Rafee_Rahimi <135032547+RafeeHabibi@users.noreply.github.com>
2024-01-18 22:01:44 +0430

    Add files via upload
    Add your self : package.json and modules

---
+++ step.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+
+function Step()
+{
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+  ];
+
+  const[step, setStep] = useState(1);
+
+  function handlePrevious()
+  {
+    if(step > 1) setStep(step - 1);
+  }
+  function handleNext()
+  {
+    if(step < 3) setStep(step + 1);
+  }
+
+    return(
+        <div className="steps">
+            <div className="numbers">
+                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+            </div>
+            <p className="message">Step {step} : {messages[step - 1]}</p>
+
+            <div className="buttons">
+                <button onClick={handlePrevious}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+            </div>
+        </div>
+    );
+}
+
+export default Step;
